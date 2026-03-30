1e65f8a2b36de38e496393b0964fbfd89e640892
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-30 09:52:31 +0530

    feat: add jest-dom dependency

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,17 +1,18 @@
-import { vi } from "vitest";
- 
+import { vi } from 'vitest';
+import '@testing-library/jest-dom';
+
 type LinkProps = {
   children: unknown;
 };
- 
-vi.mock("next/navigation", () => ({
+
+vi.mock('next/navigation', () => ({
   useRouter: () => ({
     push: vi.fn(),
     replace: vi.fn(),
   }),
 }));
- 
-vi.mock("next/link", () => {
+
+vi.mock('next/link', () => {
   return {
     default: (props: LinkProps) => props.children,
   };
